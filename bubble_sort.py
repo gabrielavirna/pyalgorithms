4e6177f5336f33fc861234584dc6dbfa6e1efc90
Gabriela Virna <gabrielavirna012@gmail.com>
2016-08-29 15:36:23 -0700

    Improved efficiency of bubbleSort algorithm

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,18 +1,16 @@
 
 def bubbleSort(elementsList):
     n = len(elementsList)
-    swapped = True
-    while n > 0 and swapped:
+    for j in range(0,n-1):
         swapped = False
-        for i in range(0,n-1):
+        for i in range(0,n-1-j):
             if elementsList[i] > elementsList[i+1]:
-                swapped = True
                 tmp = elementsList[i]
                 elementsList[i]= elementsList[i+1]
                 elementsList[i+1] = tmp
-                print('The swapped elements are:', elementsList[i],'and',elementsList[i+1])
-                print(elementsList)
-        n = n - 1
+                swapped = True
+        if not swapped: break
+    return elementsList
 
 
 elementsList = [1,4,5,2,7,0,13,9]

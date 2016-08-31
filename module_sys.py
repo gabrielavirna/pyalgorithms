1c0967791d28452cdf22eedf608afdb314efbd39
gabrielavirna <gabrielavirna012@gmail.com>
2016-08-31 14:37:33 -0700

    Added modules

---
+++ module_sys.py
@@ -0,0 +1,14 @@
+# system input,output
+import sys
+
+#sys.stderr.write('This is stderr text\n') # prints in red
+#sys.stderr.flush()
+#sys.stdout.write('This is stdout text\n')   # prints in black
+
+#print(sys.argv) # file path to the current editing file
+
+#if len(sys.argv) > 1:
+#    print(sys.argv[1])
+
+if len(sys.argv) > 1:
+    print(float(sys.argv[1]) + 5)
